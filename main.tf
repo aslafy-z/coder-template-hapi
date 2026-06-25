f3f924058ac28cdb4694ca69bf657d38f84a546a
Zadkiel AHARONIAN <hello@zadkiel.fr>
2026-06-25 14:07:19 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ data "coder_parameter" "workspace_image" {
   description  = "Pre-built workspace image to run for this template. Release automation updates this default before tagging."
   type         = "string"
   mutable      = true
-  default      = "ghcr.io/aslafy-z/coder-template-hapi:v0.0.10"
+  default      = "ghcr.io/aslafy-z/coder-template-hapi:v0.0.9"
   order        = 1
 }
 
